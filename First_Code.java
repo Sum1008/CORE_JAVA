fa464500f5b2ad453223cd7b7bef5b66f363ca31
Sum1008 <sumitchougule2214@gmail.com>
2024-04-29 17:47:43 +0530

    Add First_Code.java

---
+++ First_Code.java
@@ -0,0 +1,10 @@
+public class First_Code {
+    public static void main(String [] args){
+        System.out.println("I am Sumit Chougule.");
+        System.out.println("I am a Software Enginner.");
+        System.out.println("Here you will get best repo for Core java to learn ."); 
+        System.out.println("Keep Learning and Keep Shining");
+        
+        
+    }
+}
